aa2bdf414155aa8f76fb9420fb7d1f0b17998c86
diniputri20 <putridini509@gmail.com>
2022-03-09 15:26:51 +0700

    smpn 5 tegal

--- app/src/main/java/com/example/smpn5tegal/EkskulActivity.java
+++ app/src/main/java/com/example/smpn5tegal/EkskulActivity.java
@@ -4,11 +4,30 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 public class EkskulActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.sklh1,
+            R.drawable.sklh2,
+            R.drawable.sklh3,
+            R.drawable.sklh4,
+            R.drawable.sklh3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ekskul);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
     }
 }
